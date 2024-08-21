fdaf49d3cacadd701bb0b1ff7672e8c4763c21db
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2024-08-21 22:29:24 +0500

    built version

--- password_manager/settings.py
+++ password_manager/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tailwind',
     'theme',
-    'django_browser_reload',
+    # 'django_browser_reload',
 ]
 
 TAILWIND_APP_NAME = 'theme' 
